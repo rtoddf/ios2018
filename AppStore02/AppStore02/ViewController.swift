00bab411a3e95e5e9124ac3b93826126549dcbb8
Tod Fleeman <rtoddf@me.com>
2018-02-26 12:07:13 -0500

    doing app store exercies again from scratch

---
+++ AppStore02/AppStore02/ViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class FeaturedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .white
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+}
---
+++ AppStore02/AppStore02/Views/CategoryCell.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class CategoryCell:UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews(){
+        backgroundColor = .orange
+    }
+}
