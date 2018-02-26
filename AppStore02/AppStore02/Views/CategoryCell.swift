852408c96cb8f0a14a55826d644bfbae16d45fd1
Tod Fleeman <rtoddf@me.com>
2018-02-26 16:18:51 -0500

    finished apps exercise 1

--- AppStore02/AppStore02/Views/CategoryCell.swift
+++ AppStore02/AppStore02/Views/CategoryCell.swift
@@ -32,12 +32,28 @@ class CategoryCell:UICollectionViewCell, UICollectionViewDataSource, UICollectio
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: frame.height)
+        return CGSize(width: 100, height: frame.height - 30)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
     }
+    
+    let categoryTitleLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Actors"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = UIColor(hexString: "#333333")
+        return label
+    }()
+    
+    let dividerBarView:UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(hexString: "#baba71")
+        return view
+    }()
     
     func setupViews(){
         backgroundColor = .white
@@ -47,7 +63,23 @@ class CategoryCell:UICollectionViewCell, UICollectionViewDataSource, UICollectio
         peopleCollectionView.delegate = self
         peopleCollectionView.register(AppCell.self, forCellWithReuseIdentifier: appCellId)
         
+        addSubview(categoryTitleLabel)
+        addSubview(dividerBarView)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]-14-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": categoryTitleLabel]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": peopleCollectionView]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": peopleCollectionView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]-14-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": dividerBarView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-4-[v2(30)]-4-[v0][v1(0.5)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": peopleCollectionView, "v1": dividerBarView, "v2": categoryTitleLabel]))
     }
 }
+
+
+
+
+
+
+
+
+
+
+
