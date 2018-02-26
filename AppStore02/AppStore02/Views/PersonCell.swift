a2705dd73a14ff75341960456591cfa2f2dd3737
Tod Fleeman <rtoddf@me.com>
2018-02-26 17:47:12 -0500

    tweaks before data souyrce

--- AppStore02/AppStore02/Views/PersonCell.swift
+++ AppStore02/AppStore02/Views/PersonCell.swift
@@ -4,12 +4,12 @@ class PersonCell:UICollectionViewCell {
     var person:Person? {
         didSet {
             guard let name = person?.name else { return }
-            guard let bio = person?.bio else { return }
+            guard let shiortBio = person?.bio else { return }
             guard let birthDate = person?.birthDate else { return }
             guard let imageName = person?.imageName else { return }
             
             personNameLabel.text = name
-            personDescriptionLabel.text = bio
+            personShortBioLabel.text = shiortBio
             personBirthdateLabel.text = birthDate
             personImageView.image = UIImage(named: imageName)
         }
@@ -26,7 +26,6 @@ class PersonCell:UICollectionViewCell {
     
     let personImageView:UIImageView = {
         let personIV = UIImageView()
-        personIV.image = UIImage(named: "tom-daley")
         personIV.contentMode = .scaleAspectFill
         personIV.clipsToBounds = true
         return personIV
@@ -34,15 +33,13 @@ class PersonCell:UICollectionViewCell {
     
     let personNameLabel:UILabel = {
         let label = UILabel()
-        label.text = "Tom Daley"
         label.font = UIFont.boldSystemFont(ofSize: 13)
         label.numberOfLines = 2
         return label
     }()
     
-    let personDescriptionLabel:UILabel = {
+    let personShortBioLabel:UILabel = {
         let label = UILabel()
-        label.text = "British driver"
         label.font = UIFont.systemFont(ofSize: 12)
         label.textColor = UIColor(hexString: "#333333")
         return label
@@ -50,23 +47,20 @@ class PersonCell:UICollectionViewCell {
     
     let personBirthdateLabel:UILabel = {
         let label = UILabel()
-        label.text = "21 May 1994"
         label.font = UIFont.systemFont(ofSize: 12)
         label.textColor = UIColor(hexString: "#666666")
         return label
     }()
     
     func setupViews(){
-//        backgroundColor = .orange
-        
         addSubview(personImageView)
         addSubview(personNameLabel)
-        addSubview(personDescriptionLabel)
+        addSubview(personShortBioLabel)
         addSubview(personBirthdateLabel)
         
         personImageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
         personNameLabel.frame = CGRect(x: 0, y: frame.width + 2, width: frame.width, height: 20)
-        personDescriptionLabel.frame = CGRect(x: 0, y: frame.width + 18, width: frame.width, height: 22)
+        personShortBioLabel.frame = CGRect(x: 0, y: frame.width + 18, width: frame.width, height: 22)
         personBirthdateLabel.frame = CGRect(x: 0, y: frame.width + 34, width: frame.width, height: 22)
     }
 }
