4871b8f463bc97c552de922f10939eaefaf74c4d
Tod Fleeman <rtoddf@me.com>
2018-03-15 19:05:57 -0400

    working on the detail article pages

--- Events01/Events01/Controllers/ArticleDetailContoller.swift
+++ Events01/Events01/Controllers/ArticleDetailContoller.swift
@@ -11,12 +11,12 @@ class ArticleDetailController:UICollectionViewController, UICollectionViewDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("article detail: \(article)")
-        
+
         collectionView?.backgroundColor = .white
         collectionView?.alwaysBounceVertical = true
         collectionView?.register(DetailCell.self, forCellWithReuseIdentifier: cellId)
+        
+        self.navigationController?.navigationBar.tintColor = .white
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -40,10 +40,20 @@ class DetailCell:BaseCell {
             guard let leadImage = article?.lead_image else { return }
             guard let headline = article?.headline else { return }
             guard let author = article?.author else { return }
+            guard let fullText = article?.full_text else { return }
 
             leadImageView.loadImageUsingUrlString(imageUrl: leadImage)
             headlineLabel.text = headline
             authorLabel.text = author
+            
+            var html = "<style>p{margin:10px}</style>"
+            html.append(fullText)
+            
+            let rawHTML = Data(html.utf8)
+
+            guard let attributedString = try? NSMutableAttributedString(data: rawHTML, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) else { return }
+            let attributedText = NSMutableAttributedString(string: attributedString.string, attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14), NSAttributedStringKey.foregroundColor: UIColor.darkGray])
+            textView.attributedText = attributedText
         }
     }
     
@@ -69,16 +79,29 @@ class DetailCell:BaseCell {
         return label
     }()
     
+    let textView:UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.systemFont(ofSize: 14)
+//        textView.textContainerInset = .zero
+        textView.contentInset = UIEdgeInsetsMake(0, -5, 0, 0)
+        textView.isSelectable = false
+        textView.isEditable = false
+        return textView
+    }()
+    
     override func setupViews() {
         addSubview(leadImageView)
         addSubview(headlineLabel)
         addSubview(authorLabel)
+        addSubview(textView)
+        
         
         addConstraintsWithFormat(format: "H:|[v0]|", views: leadImageView)
         addConstraintsWithFormat(format: "H:|-14-[v0]-14-|", views: headlineLabel)
         addConstraintsWithFormat(format: "H:|-14-[v0]-14-|", views: authorLabel)
+        addConstraintsWithFormat(format: "H:|-14-[v0]-14-|", views: textView)
         
-        addConstraintsWithFormat(format: "V:|[v0(200)]-8-[v1]-4-[v2]", views: leadImageView, headlineLabel, authorLabel)
+        addConstraintsWithFormat(format: "V:|[v0(200)]-8-[v1(50)]-4-[v2(20)]-8-[v3]|", views: leadImageView, headlineLabel, authorLabel, textView)
     }
 }
 
