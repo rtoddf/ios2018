a6d44d65cbf6b0847753949f8659defaacd3c6c6
Tod Fleeman <rtoddf@me.com>
2018-03-20 17:35:34 -0400

    continuing work on events03

--- Events03/Events03/Controllers/EventsController.swift
+++ Events03/Events03/Controllers/EventsController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class EventsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    var articles:[Article]?
     let cellId = "cellId"
     
     override func viewDidLoad() {
@@ -10,6 +11,14 @@ class EventsController: UICollectionViewController, UICollectionViewDelegateFlow
         collectionView?.register(ArticleCell.self, forCellWithReuseIdentifier: cellId)
         
         navigationItem.title = "Movie Reviews"
+
+        Article.downloadData { (articles) in
+            self.articles = articles
+            self.collectionView?.reloadData()
+        }
+        
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -18,11 +27,24 @@ class EventsController: UICollectionViewController, UICollectionViewDelegateFlow
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ArticleCell
+        cell.article = articles?[indexPath.item]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
+        return CGSize(width: view.frame.width, height: 130)
     }
+    
+//    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        guard let article = articles?[indexPath.item] else { return }
+//        showArticleDetail(article: article)
+//    }
+//    
+//    func showArticleDetail(article:Article) {
+//        let layout = UICollectionViewFlowLayout()
+//        let articleDetailViewController = ArticleDetailController(collectionViewLayout: layout)
+//        articleDetailViewController.article = article
+//        navigationController?.pushViewController(articleDetailViewController, animated: true)
+//    }
 }
 
--- Events03/Events03/Views/ArticleCell.swift
+++ Events03/Events03/Views/ArticleCell.swift
@@ -1,9 +1,72 @@
 import UIKit
 
 class ArticleCell:BaseCell {
+    var article:Article? {
+        didSet {
+            guard let leadImage = article?.lead_image else { return }
+            guard let headline = article?.headline else { return }
+            guard let publishedDate = article?.pub_date else { return }
+            guard let author = article?.author else { return }
+            guard let summary = article?.summary else { return }
+            
+            leadImageView.loadImageUsingUrlString(imageUrl: leadImage)
+            headlineLabel.text = headline
+            var authorText = "By "
+            authorText.append(author)
+//            authorLabel.text = authorText
+//            dateLabel.text = publishedDate
+            
+            let attributedText = NSMutableAttributedString(string: summary, attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14), NSAttributedStringKey.foregroundColor: UIColor.darkGray])
+            textLabel.attributedText = attributedText
+        }
+    }
+    
+    let headlineLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.text = "This is the headline"
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    let textLabel:UILabel = {
+        let label = UILabel()
+        label.text = "This is the headline"
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    let leadImageView:UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = UIColor(hexString: "#333333")
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     override func setupViews() {
-        backgroundColor = UIColor(hexString: "#baba71")
+        backgroundColor = UIColor(hexString: "#dedede")
+        
+        addSubview(headlineLabel)
+        addSubview(textLabel)
+        addSubview(leadImageView)
+        
+        let imageWidth = frame.width / 3
+        let textWidth = imageWidth * 2
+        
+        addConstraintsWithFormat(format: "H:|-16-[v0]-16-[v1(\(imageWidth))]-16-|", views: headlineLabel, leadImageView)
+        addConstraintsWithFormat(format: "V:|-16-[v0]-8-[v1]", views: headlineLabel, textLabel)
+        addConstraintsWithFormat(format: "H:|-16-[v0(\(textWidth))]", views: textLabel)
+        addConstraintsWithFormat(format: "V:|-16-[v0]-16-|", views: leadImageView)
+        
+        // top constraint
+//        addConstraint(NSLayoutConstraint(item: textLabel,  attribute: .top, relatedBy: .equal, toItem: headlineLabel, attribute: .bottom, multiplier: 1, constant: 2))
+//        // left constraint
+//        addConstraint(NSLayoutConstraint(item: textLabel,  attribute: .left, relatedBy: .equal, toItem: headlineLabel, attribute: .left, multiplier: 0, constant: 0))
+//        // right constraint
+//        addConstraint(NSLayoutConstraint(item: textLabel,  attribute: .right, relatedBy: .equal, toItem: headlineLabel, attribute: .right, multiplier: 0, constant: 0))
+//        // height constraint
+//        addConstraint(NSLayoutConstraint(item: textLabel,  attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 100))
     }
     
 }
