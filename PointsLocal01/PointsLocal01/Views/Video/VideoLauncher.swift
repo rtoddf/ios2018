69fd27edd68fdc1a35815cfba73785e192c931ad
Todd Fleeman <Todd.Fleeman@coxinc.com>
2018-06-06 18:05:29 -0400

    added play and pause

--- PointsLocal01/PointsLocal01/Views/Video/VideoLauncher.swift
+++ PointsLocal01/PointsLocal01/Views/Video/VideoLauncher.swift
@@ -2,24 +2,92 @@ import UIKit
 import AVFoundation
 
 class VideoPlayerView:UIView {
+    
+    let activityIndicatorView:UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        aiv.translatesAutoresizingMaskIntoConstraints = false
+        aiv.startAnimating()
+        return aiv
+    }()
+    
+    lazy var pausePlayButton:UIButton = {
+        let btn = UIButton(type: .system)
+        let image = UIImage(named: "pause")
+        btn.setImage(image, for: .normal)
+        btn.tintColor = .white
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        btn.isHidden = true
+        return btn
+    }()
+    
+    var isPLaying:Bool = false
+    
+    @objc func handlePause(){
+        if isPLaying {
+            player?.pause()
+            pausePlayButton.setImage(UIImage(named: "play"), for: .normal)
+        } else {
+            player?.play()
+            pausePlayButton.setImage(UIImage(named: "pause"), for: .normal)
+        }
+
+        isPLaying = !isPLaying
+    }
+    
+    let controlsContainerView:UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .orange
+        backgroundColor = .black
+        setupPlayerView()
         
+        controlsContainerView.frame = frame
+        addSubview(controlsContainerView)
+        
+        controlsContainerView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0).isActive = true
+        
+        controlsContainerView.addSubview(pausePlayButton)
+        pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0).isActive = true
+        pausePlayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    var player:AVPlayer?
+    
+    func setupPlayerView(){
         let urlString = "http://rtodd.net/swift/video/wildlife.mp4"
         if let url = NSURL(string: urlString) {
-            let player = AVPlayer(url: url as URL)
+            player = AVPlayer(url: url as URL)
             let playerLayer = AVPlayerLayer(player: player)
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
-            player.play()
+            player?.play()
+            
+            
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
         }
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicatorView.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            pausePlayButton.isHidden = false
+            isPLaying = true
+        }
     }
 
 }
